5b8a5794d2e16c7c119b303869bdc361cacd899d
Jana <yanix2x2@gmail.com>
2025-01-16 13:46:04 +0300

    improve names

--- friend-invitation.py
+++ friend-invitation.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 friend_name = 'мой друг'
 my_name = 'Jana'
 website = 'https://dvmn.org/profession-ref-program/yanix2x2/ya6Ge/'
+my_email = 'YanixAvatar@yandex.ru'
+friend_email = 'YanixAvatar@yandex.ru'
 
 letter = """\
-From: YanixAvatar@yandex.ru
-To: YanixAvatar@yandex.ru
+From: {my_email}
+To: {friend_email}
 Subject: Приглашение!
 Content-Type: text/plain; charset="UTF-8";
 
@@ -28,7 +30,8 @@ Content-Type: text/plain; charset="UTF-8";
 
 Регистрируйся → {website}  
 На курсы, которые еще не вышли, можно подписаться и получить уведомление о релизе сразу на имейл."""\
-	.format(friend_name=friend_name, my_name=my_name, website=website).encode()
+	.format(friend_name=friend_name, my_name=my_name, website=website, my_email=my_email, friend_email=friend_email)\
+	.encode()
 
 load_dotenv(override=True)
 login = os.getenv("LOGIN")
@@ -36,5 +39,5 @@ password = os.getenv("PASSWORD")
 
 server = smtplib.SMTP_SSL('smtp.yandex.ru:465')	
 server.login(login, password)
-server.sendmail('YanixAvatar@yandex.ru', 'YanixAvatar@yandex.ru', letter)
+server.sendmail(my_email, friend_email, letter)
 server.quit()
